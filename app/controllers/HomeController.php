0e30796c38157b6e84053b019a686534a5b0402e
darciosoares <darciosoares28@gmail.com>
2023-05-17 10:25:48 -0300

    Tenstando carregamento de template no controller e criando rotas POSTs

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,21 +1,37 @@
 <?php 
 
 namespace app\controllers;
+use app\core\Request;
 
-class HomeController
+class HomeController extends Controller
 {
     public function index()
     {
-        dd("index homeController");
+        $this->view('teste');
     }
 
     public function teste($params)
     {
-        dd("teste -> homeController", $params);
+        dd("teste -> homeController", "");
     }
 
     public function teste1($params)
     {
-        dd("teste -> ", $params);
+        dd("teste -> ", $params, Request::queryAll());
+    }
+
+    public function teste2($params)
+    {
+        $nome = 'darcio de sousa';
+        //$this->view('home',["nome"=>"darcio"]);
+        $this->view('home',compact('nome'));
+
+    }
+
+    public function store($params)
+    {
+
+        dd(Request::excepts(['age','name']));
+
     }
 }
--- app/routes/Routes.php
+++ app/routes/Routes.php
@@ -7,11 +7,15 @@ class Routes {
     public static function get()
     {
         return [
-            'get' => [
+            "get" => [
                 "/" => 'HomeController@index',
+                "/teste" => 'HomeController@teste2',
                 "/teste/[0-9]+" => 'HomeController@teste',
                 "/teste/[a-z]+/sobrenome/[a-z]+" => 'HomeController@teste1',
                 "/user/[0-9]+" => 'UserController@index'
+            ],
+            "post" => [
+                "/teste" => 'HomeController@store'
             ]
         ];
     }
