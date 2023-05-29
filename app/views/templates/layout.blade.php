e054d7dfb23d82a0bd6f14f498c6f1e1b56db3f5
darciosoares <darciosoares28@gmail.com>
2023-05-29 12:27:21 -0300

    Criando form para criaçao de usuario, add axios no layout,  e baixando ele na pasta public.

--- app/views/templates/layout.blade.php
+++ app/views/templates/layout.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="@php echo PATH_PUBLIC.'css/style.css'@endphp">
-    <link rel="stylesheet" href="@php echo PATH_PUBLIC.'bootstrap/css/bootstrap.css'@endphp">
+    <link rel="stylesheet" href="@php echo PATH_PUBLIC.'bootstrap/css/bootstrap.css'@endphp">    
 </head>
 <body>
     <div class="topnav" id="myTopnav">
@@ -21,5 +21,7 @@
 
     <script src="{{PATH_PUBLIC}}js/index.js"></script>
     <script src="{{PATH_PUBLIC}}bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="{{PATH_PUBLIC}}js/axios_1.4.min.js"></script>
+  @stack('script')  
 </body>
 </html>
--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-8  m-auto">
 
-            <div>
+            <div style="text-align: center;">
                 <h1>
                     Criação e listagem de usuario
                 </h1>
@@ -14,15 +14,56 @@
 
             <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  <h2 class="accordion-header" >
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"     style="background: var(--color--purple); color:white;">
                       Accordion Item #1
                     </button>
                   </h2>
-                  <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                  {{-- add class show na div id="collapseOne" --}}
+                  <div id="collapseOne" class="accordion-collapse collapse " data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                      <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+
+                      <form id="form-create-user" enctype="multipart/form-data">
+                        <div class="row mb-3">
+                            <div class="col-12 col-sm-6">
+                              <label for="name">Nome</label>
+                              <input type="text" class="form-control" placeholder="Nome"  name="name">
+                            </div>
+                            <div class="col-12 col-sm-6">
+                              <label for="name">Email</label>
+                              <input type="email" class="form-control" placeholder="Email"  name="email">
+                            </div>
+                          </div>
+
+                          <div class="row mb-3">
+                            <div class="col-12 col-sm-6">
+                              <label for="password">Senha</label>
+                              <input type="text" class="form-control" placeholder="password" name="password" >
+                            </div>
+                            <div class="col-12 col-sm-6">
+                              <label for="repeat_password">Repita a Senha</label>
+                              <input type="text" class="form-control"placeholder="Repita a senha" name="repeat_password" >
+                            </div>
+                          </div>
+
+                          <div class="row mb-3">
+                            <div class="col-12 col-sm-6">
+                              <label for="professional_position">Cargo</label>
+                              <select name="professional_position" class="form-select">
+                                <option selected value="Gerente">Gerente</option>
+                                <option value="Cordenador">Cordenador</option>
+                                <option value="Assistente">Assistente</option>
+                              </select>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                              <label for="image">Imagen</label>
+                              <input type="file" class="form-control" placeholder="image" id="image" name="image" >
+                            </div>
+                          </div>
+                              <button class="btn btn-info">Enviar</button>
+                      </form>
                     </div>
+
                   </div>
                 </div>
                              
@@ -72,3 +113,27 @@
     
 
 @endsection
+@push('script')
+<script>
+  const form = document.getElementById('form-create-user')
+  
+  form.addEventListener('submit', async function(e){
+      e.preventDefault();
+      const formData = new FormData(this);
+
+        axios.post('/create-user', formData,{
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          }
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+       
+  })
+  
+</script>
+@endpush
